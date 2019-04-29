98c8453fa2e9d3f4099014e88d72d3318418bd88
dhiraj3194 <dhiraj.sriram@taistech.com>
2019-04-29 13:47:25 -0500

    push config file

--- src/extension.ts
+++ src/extension.ts
@@ -5,29 +5,36 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	let NEXT_TERM_ID = 1;
 
-	console.log("Terminals: " + (<any>vscode.window).terminals.length);
-
-	// vscode.window.onDidOpenTerminal
-	vscode.window.onDidOpenTerminal(terminal => {
-		console.log("Terminal opened. Total count: " + (<any>vscode.window).terminals.length);
-
-		(<any>terminal).onDidWriteData(data => {
-			console.log("Terminal data: ", data);
-		});
-	});
-	// vscode.window.onDidChangeActiveTerminal
-	vscode.window.onDidChangeActiveTerminal(e => {
-		console.log(`Active terminal changed, name=${e ? e.name : 'undefined'}`);
-	});
 
 	// vscode.window.createTerminal
 
-	context.subscriptions.push(vscode.commands.registerCommand('terminalTest.dcu', () => {
-        const terminal = vscode.window.createTerminal(`Ext Terminal #${NEXT_TERM_ID++}`);
-        const fileLocation = vscode.window.activeTextEditor.document.uri.fsPath;
-        terminal.sendText('dcu --put "'+fileLocation+'"--node https://ccadmin-prod-z4ua.oracleoutsourcing.com/ --username kathiravan.munusamy@taistech.com --password Petmate123');
-        vscode.window.showInformationMessage('DCU pushed ✔️');
-    }));
+	vscode.commands.registerCommand('terminalTest.dcu', () => {
+		const terminal = vscode.window.createTerminal(`Ext Terminal #${NEXT_TERM_ID++}`);
+		const fileLocation = vscode.window.activeTextEditor.document.uri.fsPath;
+		if (vscode.workspace.workspaceFolders) {
+			vscode.workspace.workspaceFolders.forEach((folder) => {
+				//new vscode.RelativePattern(folder, 'config.json')
+				vscode.workspace.findFiles('**/config.json', '**/node_modules/**', 10).then((uris) => {
+					uris.forEach((uri) => {
+						vscode.workspace.openTextDocument(uri.path).then((document) => {
+							let text = document.getText();
+							let textJSON = JSON.parse(text);
+							let username = textJSON.username;
+							let password = textJSON.password;
+							let node = textJSON.node;
+							terminal.sendText(
+								'dcu --put "' +
+									fileLocation +
+									'" --node ' + node+' --username '+username+' --password '+password
+							);
+							vscode.window.showInformationMessage('DCU pushed ✔️');
+						  });
+						
+					});
+				});
+			});
+		}
+	})
 
 
 
